a16ae555d95b7dd9704895899f922fd2f9f26dc2
iamtimbun <timothytimbun@gmail.com>
2022-07-16 22:27:19 +0100

    progress

--- index.js
+++ index.js
@@ -6,4 +6,33 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('fa-times');
         navbar.classList.toggle('active');
     }  
+
+    window.onscroll = () => {
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('active');
+    }
+
+    document.querySelector('#search-icon').onclick = () => {
+        document.querySelector('#search-form').classList.toggle('active');
+    }
+
+    document.querySelector('#close').onclick = () => {
+        document.querySelector('#search-form').classList.remove('active');
+    }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+var swiper = new Swiper('.home-slider', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    loop: true,
+  });
 })
