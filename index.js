4b1dfc3a38e9f8cf229563b32ef2aaccebc06853
iamtimbun <timothytimbun@gmail.com>
2022-07-18 23:17:46 +0100

    progress again

--- index.js
+++ index.js
@@ -36,3 +36,42 @@ var swiper = new Swiper('.home-slider', {
     loop: true,
   });
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    var swiper = new Swiper('.review-slider', {
+        spaceBetween: 20,
+        centeredSlides: true,
+        autoplay: {
+            delay: 7500,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            640: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        }
+      });
+   
+
+  
+        function loader() {
+            document.querySelector('.loader-container').classList.add('fade-out');
+        }
+
+        function fadeOut() {
+            setInterval(loader, 3000);
+        }
+        window.onload = fadeOut;
+
+    })
+     
